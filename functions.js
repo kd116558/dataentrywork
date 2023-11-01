e2fd5db4573ebcb967649eedc860ed75ad92f8fe
KARAN DEEP <148671295+kd116558@users.noreply.github.com>
2023-11-01 10:51:51 +0530

    Javascript file

---
+++ functions.js
@@ -0,0 +1,22 @@
+
+function home() {
+    window.location.assign("index.html").void
+
+}
+
+function about() {
+    window.location.assign("about.html").void
+
+}
+function contact() {
+    window.location.assign("contact.html").void
+
+}
+function registration() {
+    window.location.assign("registration.html").void
+
+}
+function openempform(){
+    document.getElementById("Myform2").style.display = "none";
+}
+
